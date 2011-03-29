86c8d77773fa1fe8e52012a39114712182c01f8a
Mads Ohm Larsen <mads.ohm@gmail.com>
2011-03-29 10:14:19 +0200

    Transferring data from intent to MainActivity now works. We still need a method for sending via bluetooth.

--- DustyTubaSampleApp/src/dk/hotmovinglobster/dustytuba/sampleapp/MainActivity.java
+++ DustyTubaSampleApp/src/dk/hotmovinglobster/dustytuba/sampleapp/MainActivity.java
@@ -215,21 +215,30 @@ public class MainActivity extends Activity implements BumpAPIListener, OnCancelL
         
         /* If sending through a "send to..." dialog, we want to grab the data */
         Intent intent = getIntent();
-        Object intentData;
-        int checksum = intent.getIntExtra(INTENT_SEND_CHECKSUM, 0);
-        if(intent.getBooleanExtra(INTENT_SEND_TEXT, false)) {
-        	intentData = intent.getStringExtra(INTENT_SEND_DATA);
-        	Toast.makeText(this, (String) intentData, Toast.LENGTH_LONG).show();
-        } else if(!intent.getBooleanExtra(INTENT_SEND_TEXT, true)) {
-        	intentData = intent.getParcelableExtra(INTENT_SEND_DATA);
-        } else {
-        	intentData = new Object();
+        if(intent.hasExtra(INTENT_SEND_DATA)) {
+        	Object intentData;
+	        if(intent.getBooleanExtra(INTENT_SEND_TEXT, false)) {
+	        	intentData = (String) intent.getStringExtra(INTENT_SEND_DATA);
+	        } else if(!intent.getBooleanExtra(INTENT_SEND_TEXT, true)) {
+	        	intentData = intent.getParcelableExtra(INTENT_SEND_DATA);
+	        } else {
+	        	intentData = new Object();
+	        }
+        
+	        /* Here is a way to check the checksums after we have done bluetooth transfer */
+	        /*try {
+	            int checksum = intent.getIntExtra(INTENT_SEND_CHECKSUM, 0);
+	        	if(checksum != intentData.hashCode()) {
+	        		Toast.makeText(this, "Something went wrong while transfering data", Toast.LENGTH_LONG).show();
+	        	} else {
+	        		Toast.makeText(this, "Checksums match", Toast.LENGTH_SHORT).show();
+	        	}
+	        } catch(Exception e) {
+	        	Toast.makeText(this, "Something went wrong while transfering data", Toast.LENGTH_LONG).show();
+	        }*/
+	        
+        	/* TODO: Now we need to send it via bluetooth */
         }
-        
-        if(checksum != intentData.hashCode()) {
-    		Toast.makeText(this, "Something went wrong while transfering data", Toast.LENGTH_LONG).show();
-    	}
-        
     }
     
     @Override
--- DustyTubaSampleApp/src/dk/hotmovinglobster/dustytuba/sampleapp/ShareActivity.java
+++ DustyTubaSampleApp/src/dk/hotmovinglobster/dustytuba/sampleapp/ShareActivity.java
@@ -1,7 +1,5 @@
 package dk.hotmovinglobster.dustytuba.sampleapp;
 
-import java.util.zip.CRC32;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Parcelable;
@@ -31,31 +29,34 @@ public class ShareActivity extends Activity {
     		
     		/* Create a generic object to hold the data */
     		Intent i = new Intent(this, MainActivity.class);
-    		Object intentData;
+    		
     		if(intentType.equals("text/plain")) {
     			/* The data is text */
-    			intentData = intent.getStringExtra(Intent.EXTRA_TEXT).getBytes();
-    			i.putExtra(MainActivity.INTENT_SEND_DATA, (byte[]) intentData);
-    			i.putExtra(MainActivity.INTENT_SEND_TEXT, true);
+    			String intentData = intent.getStringExtra(Intent.EXTRA_TEXT);
+    			i.putExtra(MainActivity.INTENT_SEND_DATA, (String) intentData);
+    			i.putExtra(MainActivity.INTENT_SEND_TEXT, (boolean) true);
+    			
+    			/* Calculate the hash code of the data */
+        		/* For two objects to be equal, this must be equal */
+    			checksum = intentData.hashCode();
+    			i.putExtra(MainActivity.INTENT_SEND_CHECKSUM, checksum);
     		} else {
     			/* The data is something else (e.g. could be an image) */
-    			intentData = intent.getExtras().getParcelable(Intent.EXTRA_STREAM);
+    			Parcelable intentData = intent.getExtras().getParcelable(Intent.EXTRA_STREAM);
     			i.putExtra(MainActivity.INTENT_SEND_DATA, (Parcelable) intentData);
-    			i.putExtra(MainActivity.INTENT_SEND_TEXT, false);
+    			i.putExtra(MainActivity.INTENT_SEND_TEXT, (boolean) false);
+    			
+    			/* Calculate the hash code of the data */
+        		/* For two objects to be equal, this must be equal */
+    			checksum = intentData.hashCode();
+    			i.putExtra(MainActivity.INTENT_SEND_CHECKSUM, checksum);
     		}
  
-    		Log.v("INTENT", "IntentTest: " + intentType + " " + intentData);
+    		//Log.v("INTENT", "IntentTest: " + intentType + " " + intentData);
     		/* Show the data as well as the type */
-    		Toast.makeText(this, "Got: " + intentData + " which is " + intentType + " ", Toast.LENGTH_LONG).show();
-    		
-    		/* Calculate the hash code of the data */
-    		/* For two objects to be equal, this must be equal */
-    		checksum = intentData.hashCode();
-    		i.putExtra(MainActivity.INTENT_SEND_CHECKSUM, checksum);
+    		//Toast.makeText(this, "Got: " + intentData + " which is " + intentType + " ", Toast.LENGTH_LONG).show();
     		
     		/* Launch main activity to take care of sending the data */
-    		
-    		i.putExtra(MainActivity.INTENT_SEND_DATA, true);
     		startActivity(i);
     		
     		/* Stop this activity */
