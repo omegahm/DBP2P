68dbe278d2e4fc17ff3184e8bc2d5e651e9776a5
Thomas Kjeldsen <thomas@thomaskjeldsen.dk>
2011-04-24 19:14:39 +0200

    Added MAC verification to FakeIPActivity. No RegExp needed since BluetoothAdapter.checkBluetoothAddress does the trick.

--- DustyTuba/src/dk/hotmovinglobster/dustytuba/id/FakeIPActivity.java
+++ DustyTuba/src/dk/hotmovinglobster/dustytuba/id/FakeIPActivity.java
@@ -2,10 +2,17 @@ package dk.hotmovinglobster.dustytuba.id;
 
 import dk.hotmovinglobster.dustytuba.api.BtAPI;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+/**
+ * Identity provider that simply returns the MAC address you supply to it.
+ * This enables you to use a MAC address obtained through other means.
+ * 
+ * @author Thomas
+ */
 public class FakeIPActivity extends Activity {
 
     @Override
@@ -17,18 +24,24 @@ public class FakeIPActivity extends Activity {
     	Log.i(BtAPI.LOG_TAG, "FakeIPActivity: with data (Size "+thisIntent.getExtras().size()+": "+thisIntent.getExtras().keySet()+")");
     	// Get data
         Bundle extras = thisIntent.getExtras();
-        String mac = extras.getString(BtAPI.EXTRA_IP_MAC); // TODO: grab from somewhere
+        String mac = extras.getString(BtAPI.EXTRA_IP_MAC);
         
     	Log.i(BtAPI.LOG_TAG, "FakeIPActivity: Received MAC Address: " + mac);
     	
         if (mac == null){
         	setResult(RESULT_CANCELED);
         	finish();
+        } else {
+        	mac = mac.toUpperCase();
+        	if (!BluetoothAdapter.checkBluetoothAddress(mac)){
+            	setResult(RESULT_CANCELED);
+            	finish();
+        	}
         }
         
     	// Return data
     	Intent data = new Intent();
-        data.putExtra(BtAPI.EXTRA_IP_MAC,mac); // TODO: grab from somewhere
+        data.putExtra(BtAPI.EXTRA_IP_MAC,mac);
     	setResult(RESULT_OK, data);
     	
     	Log.i(BtAPI.LOG_TAG, "FakeIPActivity: Finishing");
