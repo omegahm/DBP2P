beb74fd176848b9747fe5c32b3179f604eb3d611
jesperborgstrup <jesper@borgstrup.dk>
2011-05-18 13:23:31 +0200

    Removed Battleships libs/-folder from gitignore

--- Battleships/src/dk/hotmovinglobster/battleships/StartActivity.java
+++ Battleships/src/dk/hotmovinglobster/battleships/StartActivity.java
@@ -8,6 +8,6 @@ public class StartActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(new BattleGrid(this, Settings.GRID_WIDTH, Settings.GRID_HEIGHT));
     }
 }
