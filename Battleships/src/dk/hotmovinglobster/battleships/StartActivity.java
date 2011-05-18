a75ecfae75772035fa665cfa6644226514f34073
Thomas Kjeldsen <thomas@thomaskjeldsen.dk>
2011-05-18 14:40:49 +0200

    Added graphics. Use png files located in res/drawables/. See sexample in StartActivity. Original vector art located in res/raw/.

--- Battleships/src/dk/hotmovinglobster/battleships/StartActivity.java
+++ Battleships/src/dk/hotmovinglobster/battleships/StartActivity.java
@@ -1,13 +1,28 @@
 package dk.hotmovinglobster.battleships;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class StartActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(new BattleGrid(this, Settings.GRID_WIDTH, Settings.GRID_HEIGHT));
+        
         super.onCreate(savedInstanceState);
-        setContentView(new BattleGrid(this, Settings.GRID_WIDTH, Settings.GRID_HEIGHT));
+        // Create a new ImageView
+        ImageView imageView = new ImageView(this);
+        // Set the background color to white
+        imageView.setBackgroundColor(Color.WHITE);
+        // Screw SVG, we just use PNG.
+    	imageView.setImageResource(R.drawable.battleship_bullet);
+        imageView.setImageResource(R.drawable.battleship_ship);
+        imageView.setImageResource(R.drawable.battleship_hit);
+        // Set the ImageView as the content view for the Activity
+        setContentView(imageView);
+
     }
 }
